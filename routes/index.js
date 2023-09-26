dc79f594ee09b662488b99d90d42f148d6374ad6
Tuong Quyen Dao <daotuongquyen@gmail.com>
2023-09-26 17:03:11 -0400

    Home Page Finish

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,19 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index');
 });
-
+router.get('/about', function (req, res, next) {
+  res.render('about');
+});
+router.get('/project', function (req, res, next) {
+  res.render('project');
+});
+router.get('/services', function (req, res, next) {
+  res.render('services');
+});
+router.get('/contact', function (req, res, next) {
+  res.render('contact');
+});
 module.exports = router;
